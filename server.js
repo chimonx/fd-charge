7146abdd3aeadb6d84727bddad942e416551ed30
Kunkorn Trakulsri <118130307+chimonx@users.noreply.github.com>
2025-04-22 13:44:54 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const chargeRoute = require('/charge');
+const chargeRoute = require('./charge'); // ✅ ต้องมี './' เพื่อชี้ไปยังไฟล์ charge.js
 const app = express();
 
 app.use(express.json());
-app.use('/charge', chargeRoute);
+app.use('/charge', chargeRoute); // เช่น POST /charge
 
 app.listen(3000, () => console.log('Server running on port 3000'));
